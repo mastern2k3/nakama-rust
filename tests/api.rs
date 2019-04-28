938f3eceb075cc6a6c74988575a7635f44f74a57
nitzan <nitzan@halo-digital.com>
2019-04-28 16:14:42 +0300

    working rpc

--- tests/api.rs
+++ tests/api.rs
@@ -2,27 +2,52 @@ use nakama::api::*;
 use nakama::apigrpc::*;
 use nakama::apigrpc_grpc::*;
 
+use protobuf::well_known_types::BoolValue;
+
 use grpc::Client;
 use grpc::ClientStub;
+
+use bytes::BufMut;
+
+use uuid::Uuid;
 
 use std::sync::Arc;
 
 #[test]
-fn some_test() {
-    // let conf = grpc::ClientConf::new();
-    // let client = Arc::new(Client::new_plain("::1", 50051, Default::default()).unwrap());
-    let client = Arc::new(Client::new_plain("127.0.0.1", 7350, Default::default()).unwrap());
+fn authenticate_device_test() {
+    let client = Arc::new(Client::new_plain("::1", 7349, Default::default()).unwrap());
     let nakama_client = NakamaClient::with_client(client);
 
     let mut acc = AccountDevice::new();
 
-    acc.set_id(String::from("lol"));
+    acc.set_id(Uuid::new_v4().to_string());
 
     let mut auth_req = AuthenticateDeviceRequest::new();
 
+    let mut create = BoolValue::new();
+
+    create.set_value(true);
+
     auth_req.set_account(acc);
+    auth_req.set_create(create);
 
-    let resp = nakama_client.authenticate_device(grpc::RequestOptions::new(), auth_req);
+    let mut req_opt = grpc::RequestOptions::new();
 
-    println!("{:?}", resp.wait());
+    let mut auth_token = bytes::BytesMut::new();
+
+    auth_token.put("Basic ");
+    auth_token.put(base64::encode("defaultkey:"));
+
+    req_opt.metadata.add(
+        grpc::MetadataKey::from("authorization"),
+        auth_token.freeze(),
+    );
+
+    let resp = nakama_client.authenticate_device(req_opt, auth_req);
+
+    let dat = resp.wait();
+
+    println!("{:?}", dat);
+
+    assert!(dat.is_ok());
 }
